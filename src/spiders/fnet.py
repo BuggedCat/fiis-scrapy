bd9083055730b8651dc5720b217128eacf3f9a8f
Giancarlo Lester <gatobugado.dev@gmail.com>
2023-12-24 16:21:14 -0300

    Ajuste no nome do bucket

--- src/spiders/fnet.py
+++ src/spiders/fnet.py
@@ -23,7 +23,7 @@ class FnetDocumentsSpider(scrapy.Spider):
     custom_settings = {
         "LOG_LEVEL": "INFO",
         "FEEDS": {
-            "s3://bronze/%(name)s/reference_date=%(today)s/data.json": {
+            "s3://scrapy-fiis-bronze/%(name)s/reference_date=%(today)s/data.json": {
                 "format": "json",
                 "encoding": "utf8",
                 "indent": 0,
